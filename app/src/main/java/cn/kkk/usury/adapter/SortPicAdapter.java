6a54713e236368cfed8bb1f85c74fb8b65cff575
Fox <279099862@qq.com>
2017-05-01 01:49:34 +0800

    HomeFragment的第一个RecyclerView初步完成。

---
+++ app/src/main/java/cn/kkk/usury/adapter/SortPicAdapter.java
@@ -0,0 +1,59 @@
+package cn.kkk.usury.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+import cn.kkk.usury.R;
+import cn.kkk.usury.model.bean.SortPicBean;
+
+/**
+ * Created by apple on 2017/4/30.
+ */
+
+public class SortPicAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    Context context;
+    ArrayList<SortPicBean> arrayLlist;
+
+    public SortPicAdapter(Context context, ArrayList arrayLlist) {
+        this.context = context;
+        this.arrayLlist = arrayLlist;
+    }
+
+    @Override
+    public AppViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//            View view = View.inflate(context, R.layout.app_item, null);
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.sort_pic_item, null);
+
+        return new AppViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder parentHolder, int position) {
+        SortPicBean bean = arrayLlist.get(position);
+        AppViewHolder holder = (AppViewHolder) parentHolder;
+        holder.ivSortPic.setImageResource(bean.getPhotoId());
+
+    }
+
+
+
+    @Override
+    public int getItemCount() {
+        return arrayLlist!=null?arrayLlist.size():0;
+    }
+
+    class AppViewHolder extends RecyclerView.ViewHolder {
+        ImageView ivSortPic;
+        public AppViewHolder(View itemView) {
+            super(itemView);
+            ivSortPic = (ImageView) itemView.findViewById(R.id.iv_sortPic);
+        }
+    }
+}
--- app/src/main/java/cn/kkk/usury/view/fragment/HomeFragment.java
+++ app/src/main/java/cn/kkk/usury/view/fragment/HomeFragment.java
@@ -2,19 +2,29 @@ package cn.kkk.usury.view.fragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 import cn.kkk.usury.R;
+import cn.kkk.usury.adapter.SortPicAdapter;
+import cn.kkk.usury.model.bean.SortPicBean;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
-
+    RecyclerView mRV_SortPic;
+    StaggeredGridLayoutManager layoutManager;
+    SortPicAdapter mSortPicAdapter;
+    ArrayList<SortPicBean> mArrayListSortPic;
 
     public HomeFragment() {
     }
@@ -24,7 +34,39 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        initView(view);
+        return view;
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
+        initData();
+        layoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+        mRV_SortPic.setLayoutManager(layoutManager);
+        mRV_SortPic.setHasFixedSize(true);
+        mSortPicAdapter = new SortPicAdapter(getContext(), mArrayListSortPic);
+        mRV_SortPic.setAdapter(mSortPicAdapter);
+
+
+    }
+
+    private void initView(View view) {
+        mRV_SortPic = (RecyclerView) view.findViewById(R.id.RV_LoanSortPicture);
+    }
+
+    private void initData() {
+        mArrayListSortPic = new ArrayList<>();
+        int[] photoIs = {
+                R.drawable.baidu_safe, R.drawable.baidu_safe, R.drawable.baidu_safe,
+                R.drawable.baidu_safe, R.drawable.baidu_safe, R.drawable.baidu_safe
+        };
+        for(int i=0;i<photoIs.length;i++) {
+            SortPicBean app = new SortPicBean( photoIs[i]);
+            mArrayListSortPic.add(app);
+        }
+    }
 }
