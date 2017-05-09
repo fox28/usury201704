b755492a25af2cd023ebffc1d211c5d6e3f40fc1
Fox <279099862@qq.com>
2017-05-09 17:38:49 +0800

    添加工具类OKHttpUtils，Result，I

---
+++ app/src/main/java/cn/kkk/usury/Application/I.java
@@ -0,0 +1,58 @@
+package cn.kkk.usury.Application;
+
+public interface I {
+	// https://modelx.yuzhidushu.com/api/v1/user/temp/login
+	// https://modelx.yuzhidushu.com/api/v1/user/tickets
+	// https://modelx.yuzhidushu.com/api/v1/user/sms
+	// https://modelx.yuzhidushu.com/api/v1/user/login
+
+	String SERVER_ROOT = "https://modelx.yuzhidushu.com/api/v1/user/";
+
+	/** 临时用户登录：*/
+	String TEMPORARY_LOGIN 								= 		"temp/login";
+
+	public static interface User{
+		String MAC_UUID					=		"mac_uuid";
+	}
+
+//	public static interface User {
+//		String MAC_UUID							=		"t_superwechat_user";
+//		String USER_NAME 							= 		"m_user_name";					//用户账号
+//		String PASSWORD 							= 		"m_user_password";				//用户密码
+//		String NICK 								= 		"m_user_nick";					//用户昵称
+//	}
+//
+//	public static interface Contact {
+//		String TABLE_NAME 							= 		"t_superwechat_contact";
+//		String CONTACT_ID 							= 		"m_contact_id";					//主键
+//		String USER_NAME 							= 		"m_contact_user_name";			//用户账号
+//		String CU_NAME 								= 		"m_contact_cname";				//好友账号
+//	}
+	
+
+
+//	String SERVER_ROOT = "http://120.26.242.249:8080/SuperWeChatServerV2.0/";
+//	String SERVER_ROOT = "http://101.251.196.90:8080/SuperWeChatServerV2.0/";
+
+//	/** 上传头像图片的类型：user_avatar或group_icon */
+//	String AVATAR_TYPE 								= 		"avatarType";
+//	/** 用户的账号或群组的环信id */
+//	String NAME_OR_HXID                             =       "name_or_hxid";
+//	/** 客户端发送的获取服务端状态的请求 */
+//	String REQUEST_SERVERSTATUS 					= 		"getServerStatus";
+//	/** 客户端发送的新用户注册的请求 */
+//	String REQUEST_REGISTER		 					= 		"register";
+//	/** 客户端发送的取消注册的请求 */
+//	String REQUEST_UNREGISTER 						= 		"unregister";
+//	/** 客户端发送的用户登录请求 */
+//	String REQUEST_LOGIN 							= 		"login";
+//	/** 客户端发送的下载用户头像请求 */
+//	String REQUEST_DOWNLOAD_AVATAR	 				= 		"downloadAvatar";
+//	/** 客户端发送的上传/更新用户头像的请求 */
+//	String REQUEST_UPDATE_AVATAR 					= 		"updateAvatar";
+//	/** 客户端发送的更新用户昵称的请求 */
+//	String REQUEST_UPDATE_USER_NICK 				= 		"updateNick";
+//	/** 客户端发送的更新用户密码的请求 */
+//	String REQUEST_UPDATE_USER_PASSWORD 			= 		"updatePassword";
+
+}
