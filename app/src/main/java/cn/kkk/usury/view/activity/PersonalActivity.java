f7547e62e6a8ed7d94eb70d3ceabbc8b17386566
Fox <279099862@qq.com>
2017-05-13 17:35:51 +0800

    界面返回按钮设置

--- app/src/main/java/cn/kkk/usury/view/activity/PersonalActivity.java
+++ app/src/main/java/cn/kkk/usury/view/activity/PersonalActivity.java
@@ -3,6 +3,7 @@ package cn.kkk.usury.view.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import cn.kkk.usury.R;
@@ -12,15 +13,27 @@ import cn.kkk.usury.utils.MFGT;
 public class PersonalActivity extends AppCompatActivity {
 
     TextView mTvTitle;
+    LinearLayout mLayoutBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal);
         initView();
+        setOnListener();
+    }
+
+    private void setOnListener() {
+        mLayoutBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MFGT.finish(PersonalActivity.this);
+            }
+        });
     }
 
     private void initView() {
+        mLayoutBack = (LinearLayout) findViewById(R.id.layout_backArea);
         mTvTitle = (TextView) findViewById(R.id.tv_title);
         mTvTitle.setText(getString(R.string.personal_info));
     }
