8dd3c50850e869d109b4531457dd26fb4f40a227
Fox <279099862@qq.com>
2017-05-15 13:00:08 +0800

    PsersonalActivity个人中心界面，修改数据为从SharePreference中实时读取获得

--- app/src/main/java/cn/kkk/usury/view/activity/PersonalActivity.java
+++ app/src/main/java/cn/kkk/usury/view/activity/PersonalActivity.java
@@ -6,21 +6,30 @@ import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import cn.kkk.usury.Application.SharePreferenceUtils;
 import cn.kkk.usury.R;
 import cn.kkk.usury.utils.MFGT;
 
 
 public class PersonalActivity extends AppCompatActivity {
 
-    TextView mTvTitle;
+    TextView mTvTitle, mTvTelephone, mTvUsername;
     LinearLayout mLayoutBack;
+    String telephone, name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal);
+        initData();
         initView();
         setOnListener();
+    }
+
+    private void initData() {
+        SharePreferenceUtils.init(this);
+        telephone = SharePreferenceUtils.getInstance().getTelephone();
+        name = SharePreferenceUtils.getInstance().getName();
     }
 
     private void setOnListener() {
@@ -35,7 +44,12 @@ public class PersonalActivity extends AppCompatActivity {
     private void initView() {
         mLayoutBack = (LinearLayout) findViewById(R.id.layout_backArea);
         mTvTitle = (TextView) findViewById(R.id.tv_title);
+        mTvTelephone = (TextView) findViewById(R.id.tv_telephone);
+        mTvUsername = (TextView) findViewById(R.id.tv_username);
+
         mTvTitle.setText(getString(R.string.personal_info));
+        mTvTelephone.setText(telephone);
+        mTvUsername.setText(name);
     }
 
     public void onUpdate(View view) {
